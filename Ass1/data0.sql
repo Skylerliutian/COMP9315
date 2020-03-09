fa9daad30e3e83f26a467104547e132792656db3
Skylerliutian <skyler151096@gmail.com>
2020-03-09 19:20:39 +0800

    add some queries

--- Ass1/data0.sql
+++ Ass1/data0.sql
@@ -9,5 +9,14 @@ insert into Students(zid,name) values
 (1002,'Simth, John'), 
 (1003,'Mahagedara Patabendige,Minosha Mitsuaki Senakasiri'), 
 (1004,'I-Sun, Chen Wang'), 
-(1005,'Clifton-Everest,Charles Edward');
+(1005,'Clifton-Everest,Charles Edward'),
+(1,"O'Brien,Patrick"), 
+(2,"O'brien,Patrick"), 
+(3,'Foam-Ocean,David'), 
+(4,'Ocean-Foam,Anna-Marie'), 
+(5,"O'Brien-O'Byrne,Sean"), 
+(6,"A',O'ch-A'hop"), 
+(7,"A'-'-,O'-'");
+
+
 
