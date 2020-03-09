a828d6842427d753013374970f0d9eb4937e3a76
Skylerliutian <skyler151096@gmail.com>
2020-03-09 12:14:38 +0800

    add data0.sql

---
+++ Ass1/data0.sql
@@ -0,0 +1,12 @@
+create table Students (
+	zid    integer primary key, 
+	name   PersonName not null
+);
+insert into Students(zid,name) values 
+(1001,'Simth,John'), 
+(1002,'Simth, John'), 
+(1003,'Mahagedara Patabendige,Minosha Mitsuaki Senakasiri'), 
+(1004,'I-Sun, Chen Wang'), 
+(1005,'Clifton-Everest,Charles Edward'),
+(1006,'O'Brien, Patrick Sean');
+
