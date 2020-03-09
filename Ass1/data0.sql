fe4285b5a0f96179c7f501f98d6fb62ac49fb12c
Skylerliutian <skyler151096@gmail.com>
2020-03-09 14:18:29 +0800

    1

--- Ass1/data0.sql
+++ Ass1/data0.sql
@@ -7,6 +7,5 @@ insert into Students(zid,name) values
 (1002,'Simth, John'), 
 (1003,'Mahagedara Patabendige,Minosha Mitsuaki Senakasiri'), 
 (1004,'I-Sun, Chen Wang'), 
-(1005,'Clifton-Everest,Charles Edward'),
-(1006,"O'Brien, Patrick Sean");
+(1005,'Clifton-Everest,Charles Edward');
 
